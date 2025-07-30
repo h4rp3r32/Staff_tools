8f3db2cf074ab9c0395bb1238a5599b6a2635a05
h4rp3r32 <harperstokes1@icloud.com>
2025-07-31 02:30:28 +1000

    Delete [staff_main]/staff_nametags directory

--- [staff_main]/staff_nametags/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'h4rp3r32'
-description 'Staff name tag system for base CFX server'
-version '1.0.0'
-
-client_script 'client.lua'
-server_script 'server.lua'
