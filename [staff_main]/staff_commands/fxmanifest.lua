0971eb2566e7c5c2a9610f3d0b3a9b5c3807f518
h4rp3r32 <harperstokes1@icloud.com>
2025-08-02 02:38:41 +1000

    Add files via upload

---
+++ [staff_main]/staff_commands/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'h4rp3r32'
+description 'Staff/Admin Commands'
+version '1.1.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
